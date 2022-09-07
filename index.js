0ed81d3d669edaa463ed13a7f8f4097069be64fe
Juan Cruz Diaz <juancruzdiaz12@gmail.com>
2022-09-07 19:42:14 -0300

    agregué ARRAYS al proyecto

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let edad = prompt("Bienvenido, ingrese su edad por favor")
+/* let edad = prompt("Bienvenido, ingrese su edad por favor")
 
      if (edad >= 18){
 
@@ -9,13 +9,13 @@ let edad = prompt("Bienvenido, ingrese su edad por favor")
                          
                               switch (opcion){
                                    case 1: 
-                                        alert("Fernet")
+                                        alert("Muy bien, aquí tiene su Fernet")
                                    break
                                    case 2: 
-                                        alert("Cerveza")
+                                        alert("Muy bien, aquí tiene su Cerveza")
                                    break
                                    case 3: 
-                                        alert("Vino")
+                                        alert("Muy bien, aquí tiene su Vino")
                                    break
                                    case 4: 
                                         alert("No, de ninguna manera. Adios. ")
@@ -37,4 +37,68 @@ let edad = prompt("Bienvenido, ingrese su edad por favor")
      else
           {
                alert("Ingrese un valor valido")
-          } 
+          }  */
+
+
+// DESAFIO COMPLEMENTARIO ARRAYS
+
+let arrayAlumnosAprobados = ["Juan Cruz", "Facundo", "Benjamin"]
+
+for (let index = 0; index < arrayAlumnosAprobados.length; index++) {
+     console.log("Indice: " + index + " - " + arrayAlumnosAprobados[index])
+ }
+// AGREGO A UN ALUMNO APROBADO
+
+ arrayAlumnosAprobados.push("Tiziana")
+
+ console.log(arrayAlumnosAprobados)
+
+ // ELIMINO A UN ALUMNO PORQUE QUIERO 
+
+ arrayAlumnosAprobados.splice(2, 1)
+
+ console.log(arrayAlumnosAprobados)
+
+ // VOY A MOSTRAR LOS ALUMNOS DESAPROBADOS
+
+ let arrayAlumnosDesaprobados = ["Marcos", "Damian", "Daniel"]
+
+ console.log(arrayAlumnosDesaprobados)
+
+ // CANTIDAD TOTAL DE ALUMNOS ( APROBADOS Y DESAPROBADOS )
+
+ let arrayTotalAlumnos = arrayAlumnosAprobados.concat(arrayAlumnosDesaprobados)
+
+ console.log(arrayTotalAlumnos)
+
+ // CONCATENADOS
+
+ let arrayConcatenado = arrayTotalAlumnos.join(" | ")
+
+ console.log(arrayConcatenado)
+
+ // ¿ TIZIANA Y DAMIAN APROBARON?
+
+ console.log (arrayAlumnosAprobados.includes("Tiziana"))
+ console.log (arrayAlumnosAprobados.includes("Damian"))
+
+ // VEO QUE BENJAMIN SE CAMBIA DE CURSO
+
+ let alumnoOut = "Benjamin"
+
+ function eliminarAlumno(alumnoOut){
+     let index = arrayTotalAlumnos.indexOf(alumnoOut)
+
+     if(index != -1){
+          arrayTotalAlumnos.splice(index, 1)
+     }
+     else{
+          alert("Alumno no encontrado")
+     }
+ }
+
+ let valorAEliminar = prompt("Ingrese un nombre")
+
+ eliminarAlumno(valorAEliminar)
+
+ console.log(arrayTotalAlumnos)
